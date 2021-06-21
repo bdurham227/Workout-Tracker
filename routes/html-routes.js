dce76da622c8d7e8a4fcd61733f08edce7294a98
bdurham227 <bdurham227@gmail.com>
2021-06-21 18:35:58 -0400

    adding async function to html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 
 
-  router.get("/", (req, res) => {
+  router.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
   });
 
-  router.get("/exercise", (req, res) => {
+  router.get("/exercise", async (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
   });
 
-  router.get("/stats", (req, res) => {
+  router.get("/stats", async (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
   
