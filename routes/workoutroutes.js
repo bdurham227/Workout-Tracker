c8a3992e446b2589ab305c87fcddf0fe7f41e76b
bdurham227 <bdurham227@gmail.com>
2021-06-21 16:46:54 -0400

    added req removed { body }

--- routes/workoutroutes.js
+++ routes/workoutroutes.js
@@ -63,9 +63,9 @@ router.get('/api/workouts/range', async (req, res) => {
 });
 
 // post range
-router.post('/api/workouts/range', async ({ body }, res) => {
+router.post('/api/workouts/range', async (req, res) => {
   try {
-    const workout = await Workout.create(body);
+    const workout = await Workout.create(req.body);
   
 
     res.status(200).json(workout);
@@ -81,14 +81,15 @@ router.post('/api/workouts/range', async ({ body }, res) => {
 
 // PUT update a workout
 
-router.put('/api/workouts/:id', async ({ body, params }, res) => {
+router.put('/api/workouts/:id', async (req, res) => {
   try {
-    const workout = await Workout.findByIdAndUpdate(params.id, 
+    const workout = await Workout.findByIdAndUpdate(req.params.id, 
       {
-        $push: { exercises: body }
+        $push: { exercises: req.body }
       },
   
-      {new: true });
+      {new: true,
+      runValidators: true });
 
       const result = await workout.save();
       console.log(result);
