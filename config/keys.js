2e4aeda93f438bb730ea62a136c623d9cf64d537
bdurham227 <bdurham227@gmail.com>
2021-07-03 16:41:32 -0400

    refactor:

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:password12345@cluster0.49a2f.mongodb.net/Workout-Tracker?retryWrites=true&w=majority",
+  mongoURI:  "mongodb+srv://admin:password12345@cluster0.49a2f.mongodb.net/Workout-Tracker?retryWrites=true&w=majority",
   secretOrKey: "secret"
 
 };
