f3f28e363c79b9bee8e3714e1113e676ae60ffce
bdurham227 <bdurham227@gmail.com>
2021-07-03 16:32:18 -0400

    adding mongo_uri

--- models/Exercises.js
+++ models/Exercises.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const exerciseSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  name: {
-    type: String,
-    required: true,
-     trim: true,
-  },
-  duration: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    min: 0,
-  },
-  reps: {
-    type: Number,
-    min: 0,
-  },
-  sets: {
-    type: Number,
-    min: 0,
-  }
-});
+// const exerciseSchema = new mongoose.Schema({
+//   type: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   name: {
+//     type: String,
+//     required: true,
+//      trim: true,
+//   },
+//   duration: {
+//     type: Number,
+//     min: 0,
+//     required: true,
+//   },
+//   weight: {
+//     type: Number,
+//     min: 0,
+//   },
+//   reps: {
+//     type: Number,
+//     min: 0,
+//   },
+//   sets: {
+//     type: Number,
+//     min: 0,
+//   }
+// });
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+// const Exercise = mongoose.model('Exercise', exerciseSchema);
 
-exports.Exercise = Exercise;
+// exports.Exercise = Exercise;
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
 const express = require('express');
-
+const db = require('./config/keys').mongoURI;
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 
 // mongoose connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
-{useNewUrlParser: true,
-useUnifiedTopology: true, 
-useFindAndModify: false, 
-useCreateIndex: true,
-})
-.then(() => console.log('Connected to MongoDb!'))
-.catch(err => console.error('Erorr connecting to MongoDb', err));
+// mongoose.connect(
+//   db,
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout', 
+// {useNewUrlParser: true,
+// useUnifiedTopology: true, 
+// useFindAndModify: false, 
+// useCreateIndex: true,
+// })
+// .then(() => console.log('Connected to MongoDb!'))
+// .catch(err => console.error('Erorr connecting to MongoDb', err));
 
-
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 
 // middleware & bodyparsers
