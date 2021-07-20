062f91449a72b92a9747562008cda6905dc86b7d
bdurham227 <bdurham227@gmail.com>
2021-07-19 20:51:55 -0400

    commit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
   router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
   });
 
   router.get("/exercise", (req, res) => {
